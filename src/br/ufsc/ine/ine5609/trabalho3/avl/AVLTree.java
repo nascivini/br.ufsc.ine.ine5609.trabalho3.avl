cd1b931caf92154d33ec9f4e11071da0a2347337
Vinícius Nascimento <31168053+nascivini@users.noreply.github.com>
2018-06-06 13:11:31 -0300

    Innitial commit

---
+++ src/br/ufsc/ine/ine5609/trabalho3/avl/AVLTree.java
@@ -0,0 +1,61 @@
+
+package br.ufsc.ine.ine5609.trabalho3.avl;
+
+/**
+ *
+ * @author vinicius.nascimento
+ */
+public class AVLTree {
+    private Node mainRoot;
+    
+    public AVLTree(){
+        this.mainRoot = null;
+    }
+
+    public Node getMainRoot() {
+        return mainRoot;
+    }
+
+    public void setMainRoot(Node mainRoot) {
+        this.mainRoot = mainRoot;
+    }
+    
+    public boolean insertOnTree(int newElement){
+        if(this.mainRoot == null){
+            Node newRoot = new Node(newElement);
+            this.mainRoot = newRoot;
+            return true;
+        }
+        return false;
+    }
+    
+    public void removeFromTree(int element){
+    
+    }
+    
+    public void makeBalance(){
+        
+    }
+    
+    private int heightOfNode(Node actual){ 
+        if(actual == null){
+            return -1;
+        }
+        if(actual.getLeftChild() == null && actual.getRightChild() == null){
+            return 0;
+        }
+        else if(actual.getLeftChild() == null){
+            return 1 + heightOfNode(actual.getRightChild());
+        }
+        else if(actual.getRightChild() == null){
+            return 1 + heightOfNode(actual.getLeftChild());
+        }
+        else{
+            return 1 + Math.max(heightOfNode(actual.getLeftChild()), heightOfNode(actual.getRightChild()));
+        }
+    }
+    
+    private void updateBalance(Node node){
+        node.setBalance(heightOfNode(node.getRightChild())- heightOfNode(node.getLeftChild()));
+    }
+}
---
+++ src/br/ufsc/ine/ine5609/trabalho3/avl/Node.java
@@ -0,0 +1,46 @@
+package br.ufsc.ine.ine5609.trabalho3.avl;
+
+/**
+ *
+ * @author vinicius.nascimento
+ */
+public class Node {
+    private int root;
+    private Node leftChild;
+    private Node rightChild;
+    private int balance;
+    
+    public Node(){
+        this.leftChild = null;
+        this.rightChild = null;
+    }
+    
+    public Node(int element){
+        this.root = element;
+    }
+
+    public int getRoot() {
+        return root;
+    }
+
+    public void setRoot(int root) {
+        this.root = root;
+    }
+
+    public Node getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(Node leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public Node getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(Node rightChild) {
+        this.rightChild = rightChild;
+    }
+   
+}
